7ecde73eee235014f726341cdbca1a155fa67aa5
BCA-GANPATUNIVERSITY <ravalkishn11@gmail.com>
2025-04-25 23:49:56 -0600

    Update upload.blade.php

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,1 +1,11 @@
+<div>
+    <!-- He who is contented is rich. - Laozi -->
+     <h1>Upload file</h1>
+     <form action="upload" method="post" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="file" id="">
+        <button>Upload file</button>
+
+     </form>
+</div>
 
