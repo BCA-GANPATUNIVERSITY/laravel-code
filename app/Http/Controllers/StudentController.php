0b9a13ff96a14611d3f643b61fef989f31f1e2d6
BCA-GANPATUNIVERSITY <ravalkishn11@gmail.com>
2025-04-25 23:59:23 -0600

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,1 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    function show(){
+        return "List Of Student";
+    }
+
+    function add(){
+        return "new student added";
+    }
+
+    function delete(){
+        return "deleted  Student";
+    }
+}
 
