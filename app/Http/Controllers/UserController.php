1ea8c2f64fd2d7d5c2e7e1a9ffb64206fe2297da
BCA-GANPATUNIVERSITY <ravalkishn11@gmail.com>
2025-04-26 00:02:32 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,1 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+
+class UserController extends Controller
+{
+    //
+    
+    function adduser(Request $request){
+        $request->validate([
+            'Username'=>'required | min:3 | max:10',
+            'Email'=>'required | email',
+            'City'=>'required | max:20',
+            'skill'=>'required',
+            
+
+        ]);
+        // return $request;
+        echo "User Name Is : $request->Username";
+        echo "<br>";
+        echo "User Emai Is : $request->Email";
+        echo "<br>";
+        echo "User Address Is : $request->City";
+        echo "<br>";
+        print_r($request->skill);
+        echo "<br>";
+        echo "User City Is : $request->city";
+        echo "<br>";
+        echo "User Age Is : $request->age";
+        echo "<br>";
+
+    }
+}
 
