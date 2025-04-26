6bf0702031162e608776b103a15a9e2331b0e9d6
BCA-GANPATUNIVERSITY <ravalkishn11@gmail.com>
2025-04-25 23:58:06 -0600

    Update customercontroller.php

--- app/Http/Controllers/customercontroller.php
+++ app/Http/Controllers/customercontroller.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\facades\DB;
+               
+                            //   database ke liye
+
+class customercontroller extends Controller
+{
+    //
+    function customer(){
+        return DB::select('select * from users');
+    }
+}
 
